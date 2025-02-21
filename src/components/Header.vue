780e5fb7bdd4c18a3b5cdf97a870bed41a6e9799
HanyangZhong <hanyangzhong552@gmail.com>
2025-02-21 17:23:14 +0000

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,6 +24,10 @@ const more_paper = {
     "LLM-SAP": {
         name: "LLM-SAP - ICME 2024",
         link: "https://github.com/HanyangZhong/Situational_Planning_datasets",
+    },
+    "BRU": {
+        name: "Balancing Rigor and Utility - arXiv",
+        link: "https://arxiv.org/abs/2406.10999",
     }
 }
 
@@ -56,13 +60,13 @@ const more_paper = {
         <el-sub-menu index="2">
             <template #title>More</template>
             <el-menu-item index="2-1">
-                <a :href=more_paper.ExtDM.link>{{ more_paper.ExtDM.name }}</a>
+                <a :href=more_paper.ExtDM.link>{{ more_paper.FENet.name }}</a>
             </el-menu-item>
             <el-menu-item index="2-2">
-                <a :href=more_paper.MPOT.link>{{ more_paper.MPOT.name }}</a>
+                <a :href=more_paper.MPOT.link>{{ more_paper.LLM-SAP.name }}</a>
             </el-menu-item>
             <el-menu-item index="2-3">
-                <a :href=more_paper.ControlNet.link>{{ more_paper.ControlNet.name }}</a>
+                <a :href=more_paper.ControlNet.link>{{ more_paper.BRU.name }}</a>
             </el-menu-item>
         </el-sub-menu>
 
