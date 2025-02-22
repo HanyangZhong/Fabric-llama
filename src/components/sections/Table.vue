a6e3a6e3d30369b4be6a77af5ab55d5bd3d6dde3
HanyangZhong <hanyangzhong552@gmail.com>
2025-02-22 18:17:32 +0000

    Update Table.vue

--- src/components/sections/Table.vue
+++ src/components/sections/Table.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 
 // 表格数据
-const tableData = [
+const table1Data = [
   {
     model: "GPT-4o",
     modality: "RGB",
@@ -25,11 +25,11 @@ const tableData = [
   {
     model: "GPT-4o",
     modality: "Uncalibrated T-images + RGB",
-    elasticity: 52,
-    softness: 60,
-    thickness: 82,
-    texture: 73,
-    overall: 69,
+    elasticity: 55,
+    softness: 66,
+    thickness: 89,
+    texture: 82,
+    overall: 73,
     sk: 10.7,
   },
   {
@@ -40,7 +40,7 @@ const tableData = [
     thickness: 95,
     texture: 90,
     overall: 76,
-    sk: 1.6,
+    sk: 16,
   },
   {
     model: "Llama3.2-Vision-90B",
@@ -55,10 +55,10 @@ const tableData = [
   {
     model: "Llama3.2-Vision-90B",
     modality: "One Calibrated T-image + RGB",
-    elasticity: 30,
-    softness: 24,
-    thickness: 43,
-    texture: 29,
+    elasticity: 9,
+    softness: 26,
+    thickness: 41,
+    texture: 50,
     overall: 31.5,
     sk: 19.3,
   },
@@ -66,9 +66,9 @@ const tableData = [
     model: "Llama3.2-Vision-90B",
     modality: "Uncalibrated T-images + RGB",
     elasticity: 16,
-    softness: 22,
-    thickness: 32,
-    texture: 20,
+    softness: 2,
+    thickness: 31,
+    texture: 29,
     overall: 19.5,
     sk: 13.9,
   },
@@ -82,9 +82,84 @@ const tableData = [
     overall: 49.8,
     sk: 2.5,
   },
+]
+
+// 表格数据
+const table2Data = [
+  {
+    model: "DeepSeek Janus-Pro-7B",
+    elasticity: 28,
+    softness: 41,
+    thickness: 46,
+    texture: 40,
+    overall: 38.8,
+    sk: 84.4,
+  },
+  {
+    model: "DeepSeek Janus-Pro-7B + D-SFT",
+    elasticity: 50,
+    softness: 50,
+    thickness: 50,
+    texture: 50,
+    overall: 50,
+    sk: 100,
+  },
+  {
+    model: "Llama3.2-Vision-11B",
+    elasticity: 53,
+    softness: 53,
+    thickness: 46,
+    texture: 44,
+    overall: 49,
+    sk: 60,
+  },
+  {
+    model: "Llama3.2-Vision-11B + D-SFT",
+    elasticity: 50,
+    softness: 42,
+    thickness: 51,
+    texture: 45,
+    overall: 47,
+    sk: 31.4,
+  },
+  {
+    model: "Llama3.2-Vision-90B",
+    elasticity: 54,
+    softness: 48,
+    thickness: 48,
+    texture: 49,
+    overall: 49.8,
+    sk: 2.5,
+  },
+  {
+    model: "Llama3.2-Vision-90B + D-SFT",
+    elasticity: 95,
+    softness: 82,
+    thickness: 100,
+    texture: 95,
+    overall: 93,
+    sk: 1,
+  },
+  {
+    model: "Fabric-Janus-7B",
+    elasticity: 50,
+    softness: 47,
+    thickness: 50,
+    texture: 50,
+    overall: 47,
+    sk: 5.5,
+  },
+  {
+    model: "Fabric-Llama-11B",
+    elasticity: 51,
+    softness: 50,
+    thickness: 59,
+    texture: 52,
+    overall: 53,
+    sk: 33.1,
+  },
   {
     model: "Fabric-Llama-90B",
-    modality: "Calibrated T-images + RGB",
     elasticity: 90,
     softness: 89,
     thickness: 100,
@@ -111,14 +186,14 @@ const tableData = [
                 <el-card class="card">
 
                     <!-- 默认tab -->
-                    <el-tabs class="demo-tabs" model-value="Method A">
+                    <el-tabs class="demo-tabs" model-value="Method B">
 
                     <!-- 第一个tab -->
-                    <el-tab-pane label="Method A" name="Method A">
+                    <el-tab-pane label="Method A" name="Modality Experiment Results">
 
                         <!-- 表格数据 -->
                         <el-table 
-                            :data="tableData"
+                            :data="table1Data"
                             :default-sort="{ prop: 'overall', order: 'descending' }"
                             scrollbar-always-on
                             style="width: 100%"
@@ -134,8 +209,22 @@ const tableData = [
                         </el-table>
                     </el-tab-pane>
 
-                    <el-tab-pane label="Method B" name="Method B">
-                        Method B
+                    <el-tab-pane label="Method B" name="Finetuning Experiment Results">
+                        <!-- 表格数据 -->
+                        <el-table 
+                            :data="table2Data"
+                            :default-sort="{ prop: 'overall', order: 'descending' }"
+                            scrollbar-always-on
+                            style="width: 100%"
+                        >
+                            <el-table-column prop="model" label="Model" width="180" sortable />
+                            <el-table-column prop="elasticity" label="Elasticity ACC" min-width="120" sortable />
+                            <el-table-column prop="softness" label="Softness ACC" min-width="120" sortable />
+                            <el-table-column prop="thickness" label="Thickness ACC" min-width="120" sortable />
+                            <el-table-column prop="texture" label="Texture ACC" min-width="120" sortable />
+                            <el-table-column prop="overall" label="Overall ACC" min-width="120" sortable />
+                            <el-table-column prop="sk" label="Sk ↓" min-width="100" sortable />
+                        </el-table>
                     </el-tab-pane>
 
                     <el-tab-pane label="Method C" name="Method C">
