8eb79f96466519fd50362a940b6a61632a3b763b
HanyangZhong <hanyangzhong552@gmail.com>
2025-02-22 18:01:23 +0000

    Update Table.vue

--- src/components/sections/Table.vue
+++ src/components/sections/Table.vue
@@ -2,30 +2,96 @@
 
 // 表格数据
 const tableData = [
-{
-    model: 'Model A',
-    params: '10 B',
-    value_a: '123',
-    value_b: '321',
-},
-{
-    model: 'Model B',
-    params: '30 B',
-    value_a: '456',
-    value_b: '654',
-},
-{
-    model: 'Model C',
-    params: '20 B',
-    value_a: '789',
-    value_b: '987',
-},
-{
-    model: 'Model D',
-    params: '5 B',
-    value_a: '100',
-    value_b: '200',
-},
+  {
+    model: "GPT-4o",
+    modality: "RGB",
+    elasticity: 54,
+    softness: 61,
+    thickness: 77,
+    texture: 84,
+    overall: 69,
+    sk: 14.2,
+  },
+  {
+    model: "GPT-4o",
+    modality: "One Calibrated T-image + RGB",
+    elasticity: 54,
+    softness: 63,
+    thickness: 89,
+    texture: 85,
+    overall: 72.7,
+    sk: 12.5,
+  },
+  {
+    model: "GPT-4o",
+    modality: "Uncalibrated T-images + RGB",
+    elasticity: 52,
+    softness: 60,
+    thickness: 82,
+    texture: 73,
+    overall: 69,
+    sk: 10.7,
+  },
+  {
+    model: "GPT-4o",
+    modality: "Calibrated T-images + RGB",
+    elasticity: 55,
+    softness: 64,
+    thickness: 95,
+    texture: 90,
+    overall: 76,
+    sk: 1.6,
+  },
+  {
+    model: "Llama3.2-Vision-90B",
+    modality: "RGB",
+    elasticity: 23,
+    softness: 14,
+    thickness: 41,
+    texture: 33,
+    overall: 27.7,
+    sk: 41.8,
+  },
+  {
+    model: "Llama3.2-Vision-90B",
+    modality: "One Calibrated T-image + RGB",
+    elasticity: 30,
+    softness: 24,
+    thickness: 43,
+    texture: 29,
+    overall: 31.5,
+    sk: 19.3,
+  },
+  {
+    model: "Llama3.2-Vision-90B",
+    modality: "Uncalibrated T-images + RGB",
+    elasticity: 16,
+    softness: 22,
+    thickness: 32,
+    texture: 20,
+    overall: 19.5,
+    sk: 13.9,
+  },
+  {
+    model: "Llama3.2-Vision-90B",
+    modality: "Calibrated T-images + RGB",
+    elasticity: 54,
+    softness: 48,
+    thickness: 48,
+    texture: 49,
+    overall: 49.8,
+    sk: 2.5,
+  },
+  {
+    model: "Fabric-Llama-90B",
+    modality: "Calibrated T-images + RGB",
+    elasticity: 90,
+    softness: 89,
+    thickness: 100,
+    texture: 97,
+    overall: 94,
+    sk: 0,
+  },
 ]
 </script>
 
@@ -53,13 +119,18 @@ const tableData = [
                         <!-- 表格数据 -->
                         <el-table 
                             :data="tableData"
-                            :default-sort="{ prop: 'value_b', order: 'descending' }"
+                            :default-sort="{ prop: 'overall', order: 'descending' }"
                             scrollbar-always-on
+                            style="width: 100%"
                         >
-                            <el-table-column prop="model" label="Model" width="100" sortable/>
-                            <el-table-column prop="params" label="#Params" min-width="120" sortable/>
-                            <el-table-column prop="value_a" label="Value A" min-width="120" sortable/>
-                            <el-table-column prop="value_b" label="Value B" min-width="120" sortable/>
+                            <el-table-column prop="model" label="Model" width="180" sortable />
+                            <el-table-column prop="modality" label="Modality Data" min-width="200" />
+                            <el-table-column prop="elasticity" label="Elasticity ACC" min-width="120" sortable />
+                            <el-table-column prop="softness" label="Softness ACC" min-width="120" sortable />
+                            <el-table-column prop="thickness" label="Thickness ACC" min-width="120" sortable />
+                            <el-table-column prop="texture" label="Texture ACC" min-width="120" sortable />
+                            <el-table-column prop="overall" label="Overall ACC" min-width="120" sortable />
+                            <el-table-column prop="sk" label="Sk ↓" min-width="100" sortable />
                         </el-table>
                     </el-tab-pane>
 
